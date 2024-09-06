ed824e032f214625ded3090e7db0dce6c1edfb38
Bhavani <connect.123ofai@gmail.com>
2024-09-06 11:12:35 +0530

    init DB

--- server/main.go
+++ server/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"server/database"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
+	database.InitPostgres()
 	app := fiber.New()
-
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.JSON((fiber.Map{
 			"message": "Hello",
