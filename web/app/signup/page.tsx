080501ec1d828dcd78e75fbf0a5640de6d678a90
Bhavani <connect.123ofai@gmail.com>
2024-09-06 11:12:56 +0530

    added signup and login page

--- web/app/signup/page.tsx
+++ web/app/signup/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const Signup = () => {
@@ -8,34 +10,34 @@ const Signup = () => {
   }
 
   return (
-    <div className="bg-container">
-      <div className="chat-window">
+    <div className="">
+      <div className="">
         {/* <HomeButton /> */}
-        <form onSubmit={handleSubmit} className="form">
-          {/* <div className="headings">Let's Get Started!</div>
-          {errorMessage && (
+        <form onSubmit={handleSubmit} className="bg-foreground p-12">
+          <div className="text-3xl font-bold text-black">Lets Get Started!</div>
+          {/* {errorMessage && (
             <span className="error-message">*{errorMessage}</span>
           )} */}
           <br />
-          <div className="form-input-box">
-            <input
+          <div className="space-y-3">
+            <Input
               type="text"
-              className="form-input"
+              className="rounded-xl"
               // value={username}
               // onChange={(e) => setUsername(e.target.value)}
               placeholder="Enter your name"
             />
-            <input
+            <Input
               type="password"
-              className="form-input"
+              className="rounded-xl"
               // value={password}
               // onChange={(e) => setPassword(e.target.value)}
               placeholder="Create a Strong Password"
             />
+            <Button type="submit" className="rounded-xl w-full">
+              Signup
+            </Button>
           </div>
-          <button type="submit" className="form-button">
-            Signup
-          </button>
         </form>
       </div>
     </div>
