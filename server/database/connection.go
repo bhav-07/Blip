a83d92ca793297c20e4fc55bd3d872c831a3678e
Bhavani <connect.123ofai@gmail.com>
2024-09-06 15:13:21 +0530

    feat: added middleware

---
+++ server/database/connection.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"server/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+var DB *gorm.DB
+
+var postgresConfig PostgresConfig
+
+func init() {
+	postgresConfig = PostgresConfig{
+		Host:     config.Config.PostgresHost,
+		Port:     config.Config.PostgresPort,
+		User:     config.Config.PostgresUser,
+		Password: config.Config.PostgresPassword,
+		Database: config.Config.PostgresDatabase,
+	}
+}
+
+func InitPostgres() {
+	var err error
+	dsn := "host=" + postgresConfig.Host + " user=" + postgresConfig.User + " password=" + postgresConfig.Password + " dbname=" + postgresConfig.Database + " port=" + postgresConfig.Port + " sslmode=require"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Connected to DB")
+
+	// Migrate the schema
+	DB.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+	DB.AutoMigrate(&DBUser{}, &DBMessage{})
+}
