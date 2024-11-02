0a82876dbf4e5c6fa965c27113c09a23514ec061
Bhavani J <jangirbhavani201@gmail.com>
2024-11-02 14:35:47 +0530

    completed signup and login page

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
-};
+import { useRouter } from "next/navigation";
+import API from "../api";
+import React, { useState } from "react";
+import BlipLogo from "@/components/blip-logo";
 
-export default page;
+function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await API.post("localhost:8080/api/auth/signup", {
+        username,
+        password,
+      });
+      console.log(response);
+      if (response.data.error) {
+        console.log(response.data);
+        setErrorMessage(
+          response.data.message || "Signup failed, Please try again."
+        );
+        return;
+      }
+      console.log("Signup successful", response.data);
+      router.push("/login");
+    } catch (error) {
+      console.error("Signup failed", error);
+    }
+  };
+
+  return (
+    <div className="bg-white bg-opacity-10 backdrop-blur-lg rounded drop-shadow-lg w-[500px]">
+      <div className="text-popover rounded-lg sm:p-16 p-4 m-2 sm:m-0 text-center space-y-4">
+        {/* <HomeButton /> */}
+        <form onSubmit={handleSubmit}>
+          <div className="text-base font-semibold text-white space-y-4 flex flex-col">
+            <BlipLogo />
+            <h1 className="text-neutral-400">Login to continue</h1>
+            {errorMessage && (
+              <span className="error-message">*{errorMessage}</span>
+            )}
+            <br />
+            <div className="flex flex-col">
+              <label className="text-start pb-1 text-xl font-normal">
+                Username
+              </label>
+              <input
+                type="text"
+                className="p-3 rounded-md text-black"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-start pb-1 text-xl font-normal">
+                Password
+              </label>
+              <input
+                type="password"
+                className="p-3 rounded-md text-black"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter your password"
+              />
+            </div>
+            <button type="submit" className="bg-black rounded-md p-3">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
--- web/app/signup/page.tsx
+++ web/app/signup/page.tsx
@@ -1,47 +1,83 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import React from "react";
+import { useRouter } from "next/navigation";
+import API from "../api";
+import React, { useState } from "react";
+import axios from "axios";
+import BlipLogo from "@/components/blip-logo";
 
-const Signup = () => {
-  function handleSubmit() {
-    console.log("SUBMITTED");
-  }
+function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await API.post("/api/auth/signup", {
+        username,
+        password,
+      });
+      console.log(response);
+      if (response.data.error) {
+        console.log(response.data);
+        setErrorMessage(
+          response.data.message || "Signup failed, Please try again."
+        );
+        return;
+      }
+      console.log("Signup successful", response.data);
+      router.push("/login");
+    } catch (error) {
+      console.error("Signup failed", error);
+    }
+  };
 
   return (
-    <div className="">
-      <div className="">
+    <div className="bg-white bg-opacity-10 backdrop-blur-lg rounded drop-shadow-lg text-white w-[500px]">
+      <div className="text-popover rounded-lg sm:p-16 p-4 m-2 sm:m-0 text-center space-y-4">
         {/* <HomeButton /> */}
-        <form onSubmit={handleSubmit} className="bg-foreground p-12">
-          <div className="text-3xl font-bold text-black">Lets Get Started!</div>
-          {/* {errorMessage && (
-            <span className="error-message">*{errorMessage}</span>
-          )} */}
-          <br />
-          <div className="space-y-3">
-            <Input
-              type="text"
-              className="rounded-xl"
-              // value={username}
-              // onChange={(e) => setUsername(e.target.value)}
-              placeholder="Enter your name"
-            />
-            <Input
-              type="password"
-              className="rounded-xl"
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
-              placeholder="Create a Strong Password"
-            />
-            <Button type="submit" className="rounded-xl w-full">
+        <form onSubmit={handleSubmit}>
+          <div className="font-semibold text-white space-y-4 flex flex-col w-">
+            <BlipLogo />
+            <h1 className="text-neutral-400">Let s Get Started!</h1>
+            {errorMessage && (
+              <span className="error-message">*{errorMessage}</span>
+            )}
+            <br />
+            <div className="flex flex-col">
+              <label className="text-start pb-1 text-xl font-normal">
+                Username
+              </label>
+              <input
+                type="text"
+                className="p-3 rounded-md text-black"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-start pb-1 text-xl font-normal">
+                Password
+              </label>
+              <input
+                type="password"
+                className="p-3 rounded-md text-black"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Create a Strong Password"
+              />
+            </div>
+            <button type="submit" className="bg-black rounded-md p-3">
               Signup
-            </Button>
+            </button>
           </div>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Signup;
