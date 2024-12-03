be7f3697eabde8dea53034e390c71bf4e799bb05
Bhavani J <jangirbhavani201@gmail.com>
2024-12-03 15:14:52 +0530

    Add Kafka consumer

---
+++ kafka-consumer/config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type AppConfig struct {
+	PostgresHost     string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDatabase string
+	KafkaHost        string
+	KafkaPort        string
+	KafkaTopic       string
+	KafkaGroupID     string
+}
+
+var Config AppConfig
+
+func init() {
+	// Initialize Configuration
+	Config = AppConfig{
+		// KafkaHost:        os.Getenv("KAFKA_HOST"),
+		// KafkaPort:        os.Getenv("KAFKA_PORT"),
+		// KafkaTopic:       os.Getenv("KAFKA_TOPIC"),
+		// KafkaGroupID:     os.Getenv("KAFKA_GROUP_ID"),
+		// PostgresHost:     os.Getenv("POSTGRES_HOST"),
+		// PostgresPort:     os.Getenv("POSTGRES_PORT"),
+		// PostgresUser:     os.Getenv("POSTGRES_USER"),
+		// PostgresPassword: os.Getenv("POSTGRES_PASSWORD"),
+		// PostgresDatabase: os.Getenv("POSTGRES_DATABASE"),
+		KafkaHost:        "localhost",
+		KafkaPort:        "9092",
+		KafkaTopic:       "chat_messages",
+		KafkaGroupID:     "chat_group",
+		PostgresHost:     "localhost",
+		PostgresPort:     "5432",
+		PostgresUser:     "postgres",
+		PostgresPassword: "postgres",
+		PostgresDatabase: "chat_db",
+	}
+
+	if Config.KafkaHost == "" || Config.PostgresHost == "" {
+		log.Fatal("Environment variables not set")
+	}
+}
+
+func getEnv(key string, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
---
+++ kafka-consumer/kafka/kafka.go
@@ -0,0 +1,34 @@
+package kafka
+
+import (
+	"kafka_consumer/config"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type KafkaConfig struct {
+	Host       string
+	Port       string
+	kafkaTopic string
+	GroupID    string
+}
+
+var kafkaConfig KafkaConfig
+
+func init() {
+	kafkaConfig = KafkaConfig{
+		Host:       config.Config.KafkaHost,
+		Port:       config.Config.KafkaPort,
+		kafkaTopic: config.Config.KafkaTopic,
+		GroupID:    config.Config.KafkaGroupID,
+	}
+}
+
+func NewKafkaConsumer() *kafka.Reader {
+	kafkaBrokerAddress := kafkaConfig.Host + ":" + kafkaConfig.Port
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{kafkaBrokerAddress},
+		Topic:   kafkaConfig.kafkaTopic,
+		GroupID: kafkaConfig.GroupID,
+	})
+}
